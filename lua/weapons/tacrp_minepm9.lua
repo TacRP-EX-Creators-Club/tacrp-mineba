b72b3e7155dd356f554d2970c5d8982313017ee9
speedonerd <xztrordenaree@gmail.com>
2025-05-21 15:51:17 -0400

    pm9 evil gun

--- lua/weapons/tacrp_minepm9.lua
+++ lua/weapons/tacrp_minepm9.lua
@@ -4,27 +4,26 @@ SWEP.Spawnable = true
 AddCSLuaFile()
 
 // names and stuff
-SWEP.PrintName = "Minebea P9"
-SWEP.AbbrevName = "P9"
+SWEP.PrintName = "Minebea PM-9"
+SWEP.AbbrevName = "PM-9"
 SWEP.Category = "Tactical RP"
 
 SWEP.SubCatTier = "3Security"
-SWEP.SubCatType = "1Pistol"
+SWEP.SubCatType = "3Submachine Gun"
 
-SWEP.Description = "Japanese license-produced variant of a Swiss pistol. Exceptional accuracy and handling limited by subpar capacity."
+SWEP.Description = "Compact SMG with a stupidly high rate of fire. One-handing it isnt advisable but is pretty cool."
 SWEP.Description_Quote = "\"Ten years in the joint made you a fucking pussy!\""
 
 SWEP.Trivia_Caliber = "9x19mm"
 SWEP.Trivia_Manufacturer = "Minebea"
-SWEP.Trivia_Year = "1975"
+SWEP.Trivia_Year = "1990"
 
 SWEP.Faction = TacRP.FACTION_NEUTRAL
-SWEP.Credits = [[Model & Texture: 8sianDude
-Source Engine Port: NgHung
-Sounds: Navaro
+SWEP.Credits = [[Model & Texture: Infinity Ward
+Sounds: Infinity Ward, New World Interactive
 ]]
 
-SWEP.ViewModel = "models/weapons/tacint_shark/v_minebap9.mdl"
+SWEP.ViewModel = "models/weapons/tacint_shark/v_minepm9.mdl"
 SWEP.WorldModel = "models/weapons/tacint_shark/w_minebap9.mdl"
 
 SWEP.Slot = 1
@@ -98,9 +97,9 @@ SWEP.BodyDamageMultipliers = {
 
 // misc. shooting
 
-SWEP.Firemode = 1
+SWEP.Firemodes = {2, 1}
 
-SWEP.RPM = 420
+SWEP.RPM = 1100
 SWEP.RPMMultSemi = 0.7
 
 SWEP.Spread = 0.0015
@@ -140,27 +139,30 @@ SWEP.FreeAimMaxAngle = 2.5
 
 // hold types
 
-SWEP.HoldType = "revolver"
+SWEP.HoldType = "pistol"
 SWEP.HoldTypeSprint = "normal"
 SWEP.HoldTypeBlindFire = "pistol"
 
-SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
-SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_PISTOL
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
 
-SWEP.PassiveAng = Angle(0, 0, 0)
-SWEP.PassivePos = Vector(0, -2, -5)
+SWEP.PassiveAng = Angle(-2, 3, 0)
+SWEP.PassivePos = Vector(-1, 0, -5)
 
 SWEP.BlindFireAng = Angle(0, 5, 0)
-SWEP.BlindFirePos = Vector(0, -2, -5)
+SWEP.BlindFirePos = Vector(3, -2, -5)
 
-SWEP.BlindFireSuicideAng = Angle(-125, 0, 45)
-SWEP.BlindFireSuicidePos = Vector(25, 12, -6)
+SWEP.BlindFireSuicideAng = Angle(-135, 5, 45)
+SWEP.BlindFireSuicidePos = Vector(31, 25, -5)
 
-SWEP.SprintAng = Angle(0, 30, 0)
-SWEP.SprintPos = Vector(2, 0, -12)
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
 
-SWEP.SightAng = Angle(-0.04, 0.14, 0)
-SWEP.SightPos = Vector(-3.17, 0, -3.28)
+SWEP.SightAng = Angle(-3.9, 0.55, 4.4)
+SWEP.SightPos = Vector(-5.3, -5, -1)
+
+SWEP.CorrectivePos = Vector(1.37, 0, 0.05)
+SWEP.CorrectiveAng = Angle(2.2, -4.6, 0)
 
 SWEP.HolsterVisible = true
 SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_PISTOL
@@ -170,23 +172,23 @@ SWEP.HolsterAng = Angle(90, 0, 0)
 
 // reload
 
-SWEP.ClipSize = 9
+SWEP.ClipSize = 25
 SWEP.Ammo = "pistol"
 
-SWEP.ReloadUpInTime = 1
-SWEP.DropMagazineTime = 0.25
+SWEP.ReloadUpInTime = 1.1
+SWEP.DropMagazineTime = 0.3
 
 SWEP.ReloadTimeMult = 1
 
-SWEP.DropMagazineModel = "models/weapons/tacint_shark/magazines/minebap9.mdl"
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/uzi.mdl"
 SWEP.DropMagazineImpact = "pistol"
 
 // sounds
 
-local path = "weapons/tacint_shark/minebap9/"
+local path = "weapons/tacint_shark/minepm9/pm9_"
 
-SWEP.Sound_Shoot = "^" .. path .. "fiveseven-1.wav"
-SWEP.Sound_Shoot_Silenced = "TacRP/weapons/vertec/vertec_fire_silenced-1.wav"
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "suppressed_fp.wav"
 
 SWEP.Vol_Shoot = 110
 SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
@@ -202,12 +204,18 @@ SWEP.EjectEffect = 1
 // anims
 
 SWEP.AnimationTranslationTable = {
-    ["deploy"] = "draw",
-    ["fire_iron"] = "shoot3",
-    ["fire"] = {"shoot1", "shoot2", "shoot3"},
-    ["blind_fire"] = {"blind_shoot1", "blind_shoot2", "blind_shoot3"},
+    ["deploy"] = "deploy_1hand",
+    ["unholster"] = "unholster_1hand",
+    ["holster"] = "holster_1hand",
+    ["idle"] = "idle_1hand",
+    ["fire_iron"] = "fire1_M_1hand",
+    ["fire"] = {"fire1_M_1hand", "fire2_M_1hand", "fire3_M_1hand", "fire4_M_1hand", "fire5_M_1hand"},
+    ["blind_fire"] = {"blind_fire1", "blind_fire2", "blind_fire3"},
+    ["dryfire"] = "dryfire_1hand",
     ["melee"] = {"melee1", "melee2"},
-    ["jam"] = "midreload"
+    ["reload"] = "reload_1hand",
+    ["midreload"] = "midreload_1hand",
+    ["jam"] = "midreload_1hand"
 }
 
 SWEP.ProceduralIronFire = {
@@ -250,49 +258,46 @@ SWEP.LastShot = true
 SWEP.Attachments = {
     [1] = {
         PrintName = "Optic",
-        Category = "optic_pistol",
-        Bone = "ValveBiped.slide",
-        WMBone = "Box01",
+        Category = {"optic_cqb", "optic_medium"},
+        Bone = "ValveBiped.uzi_rootbone",
         AttachSound = "TacRP/weapons/optic_on.wav",
         DetachSound = "TacRP/weapons/optic_off.wav",
-        VMScale = 1,
-        WMScale = 1,
-        Pos_VM = Vector(0.025, 0, -0.3),
-        Ang_VM = Angle(0, 90, 180),
-        Pos_WM = Vector(0.1, -1, -1),
-        Ang_WM = Angle(0, -90, 0),
+        InstalledElements = {"optic"},
+        Pos_VM = Vector(-5.5, -0.25, -1),
+        Pos_WM = Vector(7, 1.5, -5.5),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, 0, 180),
     },
     [2] = {
         PrintName = "Muzzle",
         Category = "silencer",
-        Bone = "ValveBiped.barrel",
-        WMBone = "Box01",
+        Bone = "ValveBiped.uzi_rootbone",
         AttachSound = "TacRP/weapons/silencer_on.wav",
         DetachSound = "TacRP/weapons/silencer_off.wav",
-        VMScale = 0.45,
-        WMScale = 0.5,
-        Pos_VM = Vector(-0.25, 0, 6.2),
+        VMScale = 0.8,
+        WMScale = 1,
+        Pos_VM = Vector(-3.7, -0.25, 12.5),
         Ang_VM = Angle(90, 0, 0),
-        Pos_WM = Vector(0.1, 8.4, -1.5),
-        Ang_WM = Angle(0, -90, 0),
+        Pos_WM = Vector(18, 1.2, -3.8),
+        Ang_WM = Angle(0, 0, 180),
     },
+
     [3] = {
         PrintName = "Tactical",
         Category = {"tactical", "tactical_zoom", "tactical_ebullet"},
-        Bone = "ValveBiped.vertec_rootbone",
-        WMBone = "Box01",
+        Bone = "ValveBiped.uzi_rootbone",
         AttachSound = "TacRP/weapons/flashlight_on.wav",
         DetachSound = "TacRP/weapons/flashlight_off.wav",
-        VMScale = 0.9,
-        WMScale = 1.3,
-        Pos_VM = Vector(-2.1, -0.05, 6),
-        Ang_VM = Angle(90, 0, 180),
-        Pos_WM = Vector(0.1, 5, -2.75),
-        Ang_WM = Angle(0, -90, 180),
+        VMScale = 1,
+        WMScale = 1,
+        Pos_VM = Vector(-4, -1.1, 4),
+        Ang_VM = Angle(90, 0, -90),
+        Pos_WM = Vector(11, 1.2, -2.5),
+        Ang_WM = Angle(0, 0, 0),
     },
     [4] = {
         PrintName = "Accessory",
-        Category = {"acc", "acc_extmag_pistol", "acc_holster", "acc_brace"},
+        Category = {"acc", "acc_foldstock", "acc_extmag_smg", "acc_holster"},
         AttachSound = "TacRP/weapons/flashlight_on.wav",
         DetachSound = "TacRP/weapons/flashlight_off.wav",
     },
@@ -304,7 +309,7 @@ SWEP.Attachments = {
     },
     [6] = {
         PrintName = "Trigger",
-        Category = {"trigger_semi"},
+        Category = {"trigger_auto"},
         AttachSound = "TacRP/weapons/flashlight_on.wav",
         DetachSound = "TacRP/weapons/flashlight_off.wav",
     },
@@ -331,9 +336,7 @@ local function addsound(name, spath)
     })
 end
 
-addsound("tacint_minebap9.clip_in", path .. "clipin.wav")
-addsound("tacint_minebap9.clip_in-mid", path .. "clipin.wav")
-addsound("tacint_minebap9.clip_out", path .. "clipout.wav")
-addsound("tacint_minebap9.slide_action", path .. "slide.wav")
-addsound("tacint_minebap9.slide_shut", path .. "sliderelease.wav")
-addsound("tacint_minebap9.cock_hammer", "TacRP/weapons/vertec/vertec_cockhammer.wav")
+addsound("tacint_minepm9.insert_clip", path .. "magin.wav")
+addsound("tacint_minepm9.remove_clip", path .. "magout.wav")
+addsound("tacint_minepm9.bolt_action", path .. "boltaction.wav")
+addsound("tacint_minepm9.foldingstock_back", path .. "foldingstock_back.wav")
